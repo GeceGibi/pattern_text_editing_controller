d6aa3936781161e8bfdf72cdd9fcac01261dcef5
gece <omr.gven@gmail.com>
2024-06-13 19:52:10 +0300

    up

---
+++ lib/pattern_text_editing_controller.dart
@@ -0,0 +1,63 @@
+library pattern_text_editing_controller;
+
+import 'package:flutter/widgets.dart';
+
+part 'text_pattern.dart';
+
+class PatternTextEditingController extends TextEditingController {
+  PatternTextEditingController({required this.patterns});
+  final List<TextPattern> patterns;
+
+  @override
+  TextSpan buildTextSpan({
+    required BuildContext context,
+    TextStyle? style,
+    required bool withComposing,
+  }) {
+    final entries = <Object>[text];
+    final baseStyle = style ?? DefaultTextStyle.of(context).style;
+
+    for (final pattern in patterns) {
+      final parsedEntries = <Object>[];
+
+      for (final entry in entries) {
+        if (entry is! String) {
+          parsedEntries.add(entry);
+          continue;
+        }
+
+        entry.splitMapJoin(
+          pattern.pattern,
+          onMatch: (match) {
+            final span = pattern.builder?.call(match) ??
+                TextSpan(
+                  text: match.group(0),
+                  style: baseStyle.merge(pattern.style),
+                );
+
+            parsedEntries.add(span);
+            return '';
+          },
+          onNonMatch: (value) {
+            parsedEntries.add(value);
+            return '';
+          },
+        );
+      }
+
+      entries
+        ..clear()
+        ..addAll(parsedEntries);
+    }
+
+    return TextSpan(
+      children: entries.map((entry) {
+        if (entry is String) {
+          return TextSpan(text: entry, style: baseStyle);
+        }
+
+        return entry as TextSpan;
+      }).toList(),
+    );
+  }
+}
---
+++ lib/text_pattern.dart
@@ -0,0 +1,13 @@
+part of 'pattern_text_editing_controller.dart';
+
+class TextPattern {
+  const TextPattern({
+    required this.pattern,
+    this.builder,
+    this.style,
+  });
+
+  final RegExp pattern;
+  final TextStyle? style;
+  final TextSpan Function(Match match)? builder;
+}
